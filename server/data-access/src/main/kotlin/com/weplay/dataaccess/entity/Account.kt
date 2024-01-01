df8d3d0a103b48b943c17a645c6834c167718c77
sangholim <hybriddevslim@gmail.com>
2024-01-01 12:36:54 +0900

    WEP-12: 계정 엔티티 주석 추가

--- server/data-access/src/main/kotlin/com/weplay/dataaccess/entity/Account.kt
+++ server/data-access/src/main/kotlin/com/weplay/dataaccess/entity/Account.kt
@@ -5,6 +5,12 @@ import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 
+/**
+ * 계정 엔티티
+ *
+ * @property id 계정 일련번호
+ * @property name 계정 이름
+ */
 @Entity
 class Account(
     @Id @GeneratedValue(strategy = GenerationType.AUTO)
