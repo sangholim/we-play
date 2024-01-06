b36508bad1eea3d91b7de8804e2a5a9ac0fdbd96
sangholim <hybriddevslim@gmail.com>
2024-01-06 16:30:11 +0900

    WEP-14: admin-api > DataAccessConfiguration 수정 - JpaConfiguration 상속 받아서 처리

--- server/admin-api/src/main/kotlin/com/weplay/admin/config/DataAccessConfiguration.kt
+++ server/admin-api/src/main/kotlin/com/weplay/admin/config/DataAccessConfiguration.kt
@@ -1,16 +1,14 @@
 package com.weplay.admin.config
 
-import org.springframework.context.annotation.Bean
+import com.weplay.dataaccess.config.JpaConfiguration
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.domain.AuditorAware
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import java.util.*
 
 @Configuration
 @ComponentScan(value = ["com.weplay.dataaccess"])
-@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
-class DataAccessConfiguration {
-    @Bean
-    fun auditorProvider(): AuditorAware<String> = AuditorAware{ Optional.of("test")}
+class DataAccessConfiguration: JpaConfiguration() {
+
+    override fun auditorProvider() = AuditorAware { Optional.of("admin-api auditor") }
 }
