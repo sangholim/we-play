3974e9b843182535ba0f7499ec0b11f3e34a0c8a
ued123 <32861341+ued123@users.noreply.github.com>
2024-01-06 14:10:37 +0900

    WEP-13: 로그인 페이지 개발

--- admin-web/src/pages/login/components/LoginPage.js
+++ admin-web/src/pages/login/components/LoginPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './LoginPage.css';  
+import './LoginPage.scss';
 
 function LoginPage() {
   const [username, setUsername] = useState('');
@@ -15,17 +15,30 @@ function LoginPage() {
   };
 
   return (
-    <div class="center-block">
+    <div class="box-container">
       <form onSubmit={handleSubmit}>
-        <div>
-          <label>Username:</label>
-          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+        <div class="logo">
+          <img src={`${process.env.PUBLIC_URL}/logo.png`} alt="대체 텍스트" />
         </div>
-        <div>
-          <label>Password:</label>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <div class="login-cell">
+          <input type="text" placeholder="아이디" value={username} onChange={(e) => setUsername(e.target.value)} />
         </div>
-        <button type="submit">Login</button>
+        <div class="login-cell">
+          <input type="password" placeholder="비밀번호" value={password} onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <div class="checkbox-cell">
+          <input type="checkbox"></input><label>로그인 상태 유지</label>
+        </div>
+        <div class="login-btn-cell">
+          <button>Login</button>
+        </div>
+        <div class="add-ons">
+          <ul class="menu">
+            <li>비밀번호 찾기</li>
+            <li>아이디 찾기</li>
+            <li>회원가입</li>
+          </ul>
+        </div>
       </form>
     </div>
   );
