e06bdac1e149c80943a849a657a07f5251ae5aa0
sangholim <hybriddevslim@gmail.com>
2024-01-13 10:21:07 +0900

    WEB-16: data-migration 프로젝트 추가

---
+++ server/data-migration/build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+}
+
+dependencies {
+
+    implementation("org.liquibase:liquibase-core")
+}
---
+++ server/data-migration/src/test/kotlin/com/weplay/datamigration/DataMigrationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.weplay.datamigration
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DataMigrationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
